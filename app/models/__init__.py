c69dc0c6e8a12ae7e73316a403998af55f6bc28c
Tom Mouquet <mouquettom@gmail.com>
2026-07-29 20:22:09 +0200

    Alembic update

---
+++ app/models/__init__.py
@@ -0,0 +1,13 @@
+from app.models.client import Client
+from app.models.contract import Contract
+from app.models.employee import Employee, Role
+from app.models.event import Event
+
+
+__all__ = [
+    'Client',
+    'Contract',
+    'Employee',
+    'Role',
+    'Event',
+]
